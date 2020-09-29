79fe410ddc14ade947503521542c9bff56cb1bc9
marlon zayro <zayro8905@gmail.com>
2020-09-28 23:08:57 -0500

    add redirect button

--- src/App.js
+++ src/App.js
@@ -6,33 +6,32 @@ import Buscar_small from "./assets/images/buscar-small.png";
 
 import queryString from "query-string";
 
-import {useRoutes, A} from 'hookrouter';
-import Routes from './route'
+import { useRoutes, A } from "hookrouter";
+import Routes from "./route";
 
 import NotFound from "./components/404";
 
 function App() {
     document.body.style = "background-color: #eeeeee;";
 
-    const routeResult = useRoutes(Routes)
+    const routeResult = useRoutes(Routes);
 
     const [datos, setDatos] = useState("");
-
-
 
     useEffect(() => {
         let params = queryString.parse(document.location.search);
         console.log(params);
 
-        if (params !== "" && typeof params !== 'undefined' && Object.keys(params).length !== 0  ) {
+        if (
+            params !== "" &&
+            typeof params !== "undefined" &&
+            Object.keys(params).length !== 0
+        ) {
             console.log(params.search);
             console.log(typeof params);
             setDatos(params.search);
-
         }
     }, []);
-
-
 
     const handleInputChange = (event) => {
         // console.log(event.target.name)
@@ -42,16 +41,15 @@ function App() {
 
     return (
         <div>
-
             <div className="menu-search  menu-size">
                 <div className=" container">
                     <div className=" mx-auto ">
                         <div className="row  menu-search ">
                             <div className="col-2">
-                            <A href="/">
-                                <div className="navbar-brand-edit">
-                                <img src={Logo_small} alt="logo"></img>
-                                </div>
+                                <A href="/">
+                                    <div className="navbar-brand-edit">
+                                        <img src={Logo_small} alt="logo"></img>
+                                    </div>
                                 </A>
                             </div>
                             <div className="col-10-edit">
@@ -62,17 +60,16 @@ function App() {
                                         name="search"
                                         id="search"
                                         type="text"
-
                                         value={datos}
                                     ></input>
-                                    <button type="button" >
-                                        <A href={`/item?search=${datos}`}>
+                                    <a href={`/item?search=${datos}`}>
+                                        <button type="button">
                                             <img
                                                 src={Buscar_small}
                                                 alt="search"
                                             />
-                                        </A>
-                                    </button>
+                                        </button>
+                                    </a>
                                 </form>
                             </div>
                         </div>
@@ -80,17 +77,9 @@ function App() {
                 </div>
             </div>
 
-
-
             <div className="container">
-
-                <div className="centerProduct">
-                {routeResult || NotFound}
-                </div>
-
+                <div className="centerProduct">{routeResult || NotFound}</div>
             </div>
-
-
         </div>
     );
 }
